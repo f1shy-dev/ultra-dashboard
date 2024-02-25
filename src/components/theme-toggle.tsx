1d6de820b9ee6b6c994cee3ce9537731032a1edc
f1shy-dev <56125930+f1shy-dev@users.noreply.github.com>
2024-02-25 18:05:29 +0000

    why is it hidden

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,34 +1,38 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
-import { useTheme } from "next-themes"
+import * as React from "react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { MenubarMenu, MenubarTrigger, MenubarContent, MenubarItem } from "@/components/ui/menubar"
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+	MenubarMenu,
+	MenubarTrigger,
+	MenubarContent,
+	MenubarItem,
+} from "@/components/ui/menubar";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+	const { setTheme } = useTheme();
 
-  return (
-    <MenubarMenu>
-        <MenubarTrigger className="hidden md:block">
-                <SunIcon className="h-[1.1rem] w-[1.1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <MoonIcon className="absolute h-[1.1rem] w-[1.1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 -translate-y-[1.1rem]" />
-                <span className="sr-only">Toggle theme</span>
-        </MenubarTrigger>
-        <MenubarContent>
-          <MenubarItem onClick={() => setTheme("light")}>Light</MenubarItem>
-          <MenubarItem onClick={() => setTheme("dark")}>Dark</MenubarItem>
-          <MenubarItem onClick={()=> setTheme("system")}>System</MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-
-  )
+	return (
+		<MenubarMenu>
+			<MenubarTrigger>
+				<SunIcon className="h-[1.1rem] w-[1.1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+				<MoonIcon className="absolute h-[1.1rem] w-[1.1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 -translate-y-[1.1rem]" />
+				<span className="sr-only">Toggle theme</span>
+			</MenubarTrigger>
+			<MenubarContent>
+				<MenubarItem onClick={() => setTheme("light")}>Light</MenubarItem>
+				<MenubarItem onClick={() => setTheme("dark")}>Dark</MenubarItem>
+				<MenubarItem onClick={() => setTheme("system")}>System</MenubarItem>
+			</MenubarContent>
+		</MenubarMenu>
+	);
 }
